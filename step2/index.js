2311867bec7e8a9f4a36ba90a5e724adc21b3011
PRINCE ARYA <Parya3252@gmail.com>
2025-10-09 18:15:57 +0530

    objects st2

---
+++ step2/index.js
@@ -0,0 +1,4 @@
+isfollow = true;
+console.log(isfollow);
+name = "Prince Arya";
+console.log(name);
---
+++ step2/index2.js
@@ -0,0 +1,9 @@
+const student = {
+    name: "Prince Arya",
+    age: 20,
+    isActive: true,
+    marks: [98, 97, 95, 93]
+};
+
+console.log(student.age);
+// it is a object and object is pare of keys and values
