ff04a46ab2561bd390fbd3fcee073033d3d51f4d
PRINCE ARYA <Parya3252@gmail.com>
2025-10-10 21:26:17 +0530

    practic1

---
+++ practic work/practic1.js
@@ -0,0 +1,9 @@
+let num = prompt("Enter any number");
+if (num % 5 === 0) {
+    console.log(num + " is completely divisible by 5");
+    alert(num + " is completely divisible by 5");
+} else {
+    console.log(num + " is not completely divisible by 5");
+    alert(num + " is not completely divisible by 5");
+
+}
