d956fc1280e171123f7a3a5eba5bdf9e2a1e494f
PRINCE ARYA <Parya3252@gmail.com>
2025-10-10 21:18:15 +0530

    step5

--- step5/index3.js
+++ step5/index3.js
@@ -1,3 +1,4 @@
 let name = prompt("Enter your name: ");
 alert("Hello, " + name + "!");
+console.log(name);
 // This is a simple JavaScript program that prompts the user to enter their name and then greets them with an alert message.
