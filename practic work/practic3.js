29d852081ad33879f6d2fa11ed71fc24230c129f
PRINCE ARYA <Parya3252@gmail.com>
2025-10-10 23:06:27 +0530

    practic3

---
+++ practic work/practic3.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " is an even number.");
+    } else {
+        // console.log(i + " is an odd number.");
+    }
+}
+// This is a simple JavaScript program that prints even numbers from 1 to 100.c
+console.log("loop is ended");
