dc0da9353525c69a83c040b52087f560b9d62481
PRINCE ARYA <Parya3252@gmail.com>
2025-10-10 20:51:35 +0530

    step3up

--- step3/index2.js
+++ step3/index2.js
@@ -2,3 +2,6 @@ let c = 5;
 let d = 10;
 console.log("The sum of c and d is: " + (c + d));
 console.log("The nagative of c and d is: " + (c - d));
+// This is a simple JavaScript program that adds two numbers and prints the result to the console.
+console.log("The multiplication of c and d is: " + (c * d));
+console.log("The division of c and d is: " + (c / d));
