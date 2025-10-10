7b386c848dd90d9f6c02efecd6ad95d2fee96fac
PRINCE ARYA <Parya3252@gmail.com>
2025-10-10 21:45:52 +0530

    practic2

---
+++ practic work/practic2.js
@@ -0,0 +1,23 @@
+let mark = 78;
+// let mark = prompt("Enter your mark: ");
+if (mark >= 80 && mark <= 100) {
+    console.log("A+");
+
+} else if (mark >= 70 && mark < 80) {
+    console.log("A");
+
+} else if (mark >= 60 && mark < 70) {
+    console.log("B");
+
+} else if (mark >= 50 && mark < 60) {
+    console.log("C");
+} else if (mark >= 40 && mark < 50) {
+    console.log("D");
+} else if (mark >= 33 && mark < 40) {
+    console.log("E");
+} else {
+    console.log("F");
+
+
+
+}
