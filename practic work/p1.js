fc2120595faec2071f18d8dd463e4674461166b6
PRINCE ARYA <Parya3252@gmail.com>
2025-10-17 23:22:24 +0530

    p1.up

--- practic work/p1.js
+++ practic work/p1.js
@@ -1,1 +1,10 @@
-prompt("Good night!");
+number = prompt("Enter a number:");
+guess = 8;
+if (number == guess) {
+    console.log("Congratulations! You guessed the correct number: " + guess);
+    alert("Congratulations! You guessed the correct number: " + guess);
+
+} else {
+    console.log("Sorry, you guessed the wrong number. The correct number was: " + guess);
+    alert("Sorry, you guessed the wrong number. The correct number was: " + guess);
+}
