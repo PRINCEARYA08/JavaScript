20b00fe597ebdaf30500ef0ba3a9eac7dda05ebe
PRINCE ARYA <Parya3252@gmail.com>
2025-10-19 21:28:15 +0530

    step7

---
+++ practic work/p2.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " is even");
+
+    } else if (i === 1) {
+        console.log(i + " is neither even nor odd");
+    } else {
+        console.log(i + " is odd");
+    }
+}
---
+++ step7/index.js
@@ -0,0 +1,7 @@
+let str = "apna college";
+
+let str2 = "dusre ka college";
+
+// let str = "apna college";
+
+// let str2 = "dusre ka college";
