45ec4affeb065de62c1c99f6810ea986327da2a2
PRINCE ARYA <Parya3252@gmail.com>
2025-10-19 21:31:04 +0530

    step7.

--- step7/index.js
+++ step7/index.js
@@ -5,3 +5,8 @@ let str2 = "dusre ka college";
 // let str = "apna college";
 
 // let str2 = "dusre ka college";
+
+// for print string use =  str
+
+
+// for print and find the length of string use = str.length
