140582d4d0afefcf7e9b3f687ff8e69ca6d5bd74
PRINCE ARYA <Parya3252@gmail.com>
2025-10-19 22:14:06 +0530

    step7 up

--- step7/index.js
+++ step7/index.js
@@ -10,3 +10,12 @@ let str2 = "dusre ka college";
 
 
 // for print and find the length of string use = str.length
+
+console.log(str[0]);
+// it print 'a' because index start from 0
+
+// to find the character at index 3 use = str.charAt(3)
+
+// to find the index of string use = str.indexOf("a")
+let specialString = `this is a template literal string`;
+console.log(specialString);
---
+++ step7/index2.js
@@ -0,0 +1,16 @@
+let obj = {
+    item: "laptop",
+    price: 45000,
+    brand: "hp",
+    color: "black"
+};
+
+// to create object use = let obj = {key: value};   
+// type 1 to create object
+console.log("Item name is : ", obj.item, "there price: ", obj.price, "there brand: ", obj.brand, " or color: ", obj.color);
+// to access object properties use = obj.propertyName
+
+
+// tyepe 2 to create object
+let output = `The item name is ${obj.item} and there price is ${obj.price}`;
+console.log(output);
