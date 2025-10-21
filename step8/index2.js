8e41db6c85302d05a35c805efbff027b15775afc
PRINCE ARYA <Parya3252@gmail.com>
2025-10-21 14:03:34 +0530

    step8up

---
+++ step8/index2.js
@@ -0,0 +1,8 @@
+let a = ["red", "green", "blue", "yellow", "purple"];
+
+for (let i = 0; i < a.length - 2; i++) {
+    console.log(a[i]);
+}
+
+// this is array iteration using for loop
+// we are accessing each element of array using index number
