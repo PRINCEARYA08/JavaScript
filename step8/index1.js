f923d37a4191386922bb9e2ad67fda5f0e3a02ea
PRINCE ARYA <Parya3252@gmail.com>
2025-10-21 13:43:05 +0530

    step8

---
+++ step8/index1.js
@@ -0,0 +1,7 @@
+let names = ["alice", "bob", "charlie", "david", "eve"];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+// this is array iteration using for loop
+// we are accessing each element of array using index number
