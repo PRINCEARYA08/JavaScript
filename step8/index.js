2a693ed9734dcb18c26ce7d4f17f139f465967b8
PRINCE ARYA <Parya3252@gmail.com>
2025-10-21 13:35:02 +0530

    array

---
+++ step8/index.js
@@ -0,0 +1,7 @@
+let heros = ["ironman", "spiderman", "thor", "hulk", "captain america"];
+
+for (let i = 0; i < heros.length; i++) {
+    console.log(heros[i]);
+}
+// this is array iteration using for loop
+// we are accessing each element of array using index number
