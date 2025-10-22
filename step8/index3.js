c9098bf3f7d8194e92eebfb7e4e1b98f7b25420e
PRINCE ARYA <Parya3252@gmail.com>
2025-10-22 17:00:45 +0530

    8up

---
+++ step8/index3.js
@@ -0,0 +1,9 @@
+let cities = ["new york", "los angeles", "chicago", "houston", "phoenix"];
+
+
+for (let city of cities) {
+    console.log(city);
+}
+
+// this is array iteration using for loop
+// we are accessing each element of array using index number
