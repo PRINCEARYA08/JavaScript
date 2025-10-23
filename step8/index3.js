5cd6310345d257527ae6e97d03765ed0d4792750
PRINCE ARYA <Parya3252@gmail.com>
2025-10-23 10:33:13 +0530

    8up.

--- step8/index3.js
+++ step8/index3.js
@@ -7,3 +7,13 @@ for (let city of cities) {
 
 // this is array iteration using for loop
 // we are accessing each element of array using index number
+
+let place = ["new york", "los angeles", "chicago", "houston", "phoenix"];
+
+
+for (let city of cities) {
+    console.log(city);
+}
+
+// this is array iteration using for loop
+// we are accessing each element of array using index number
