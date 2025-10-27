d4f5194c351e264587273e6f57925974a8ca5d2d
PRINCE ARYA <Parya3252@gmail.com>
2025-10-27 17:15:57 +0530

    p2.js

--- practic work/p2.js
+++ practic work/p2.js
@@ -8,3 +8,13 @@ for (let i = 1; i <= 100; i++) {
         console.log(i + " is odd");
     }
 }
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i + " is even");
+
+//     } else if (i === 1) {
+//         console.log(i + " is neither even nor odd");
+//     } else {
+//         console.log(i + " is odd");
+//     }
+// }
