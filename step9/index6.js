c293e8bd36c48998d64fbccf1b6408a3ce11a6f8
PRINCE ARYA <Parya3252@gmail.com>
2025-12-29 14:36:31 +0530

    test10

---
+++ step9/index6.js
@@ -0,0 +1,9 @@
+let value = prompt("Enter a value:");
+if (value / 2 == 0) {
+    console.log("The value is even.");
+    alert("The value is even.");
+
+} else {
+    console.log("The value is odd.");
+    alert("The value is odd.");
+}
