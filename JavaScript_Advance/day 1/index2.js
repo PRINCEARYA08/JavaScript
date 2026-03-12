c365b8b3ecb7367444ffe446e3cebf383b08dc04
PRINCE ARYA <Parya3252@gmail.com>
2026-03-12 09:56:21 +0000

    test22

--- JavaScript_Advance/day 1/index2.js
+++
@@ -1,8 +0,0 @@
-const accountId = 123455
-let accountEmail = "prince@gmail.com"
-var accountPassword = "12345"
-accountCity = "jaipur"
-
-accountId = 2 //not allowed
-
-console.log(accountId)
---
+++ JavaScript_Advance/day1/index2.js
@@ -0,0 +1,13 @@
+const accountId = 123455
+let accountEmail = "prince@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2 //not allowed
+
+accountEmail = "huefgu@"
+accountPassword = "15456"
+accountCity = "bhopal"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
