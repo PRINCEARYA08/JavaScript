3c7057653d72c09e14ae8d6161251e256f7061ec
PRINCE ARYA <Parya3252@gmail.com>
2026-03-12 13:38:58 +0000

    dataTypes

--- JavaScript_Advance/02_dataType/index1.js
+++ JavaScript_Advance/02_dataType/index1.js
@@ -2,4 +2,26 @@
 console.log(3
     +
     3)
-console.log("Prince")
+console.log("Prince");
+
+
+
+let state;
+let age = 21;
+
+// number => 2 to power 53
+// bigint 
+// string =>""
+
+// boolean =>true/false
+
+
+//null => standalone value
+// undefined =>
+
+// symbol => unique    
+
+//object
+
+console.log(typeof age);
+console.log(typeof state);
