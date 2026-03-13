207e0f553274203fe157bc8148e2fe54199b4851
PRINCE ARYA <Parya3252@gmail.com>
2026-03-13 14:05:16 +0000

    opration

--- JavaScript_Advance/03_conversionOprations.js
+++ JavaScript_Advance/03_conversionOprations.js
@@ -1,8 +1,8 @@
-let score = "33"
-let age = 21
-let accountNumber = "BK123345"
-let value = undefined
-let desigen = true
+// let score = "33"
+// let age = 21
+// let accountNumber = "BK123345"
+// let value = undefined
+// let desigen = true
 
 
 // console.log(typeof score);
@@ -31,7 +31,32 @@ let desigen = true
 
 // string
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+/******************** Operations ************************/
+
+let value = 3 
+let negValue = -value
+// console.log(negValue)
+
+
+
+// console.log(2+2)
+// console.log(2*2)
+
+
+let str1 = "Hello"
+let str2 = " Prince"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
