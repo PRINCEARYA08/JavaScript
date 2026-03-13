987470e8c56a7884184f9be2c8e32afa0b376ebc
PRINCE ARYA <Parya3252@gmail.com>
2026-03-13 13:35:04 +0000

    03_conversionOprations

--- JavaScript_Advance/01_variable/index.js
+++
@@ -1,7 +0,0 @@
-fullName = "tony";
-
-age = 45;
-price = 99.99;
-console.log(fullName);
-console.log(age);
-console.log(price);
---
+++ JavaScript_Advance/03_conversionOprations.js
@@ -0,0 +1,28 @@
+let score = "33"
+let age = 21
+let accountNumber = "BK123345"
+let value = undefined
+let desigen = true
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false =>0
+
+
+
+
+// Boolean
+
+let isLoggedIn = 1
+let booleanIsLoggIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggIn)
