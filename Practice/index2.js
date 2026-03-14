1b7367e612667f88b6b294761dd0863905d58b55
PRINCE ARYA <Parya3252@gmail.com>
2026-03-14 02:50:18 +0000

    practice.index2

---
+++ Practice/index2.js
@@ -0,0 +1,13 @@
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     country:  "Norway"
+
+// };
+// alert(person.country)
+
+const person = {
+  name: 'John', age: 50
+};
+
+alert(person.name + ' is ' + person.age);
