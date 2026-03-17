44e05e16a5ef8531dd91b93a055665e6e803235e
PRINCE ARYA <Parya3252@gmail.com>
2026-03-17 13:51:20 +0000

    01_functions.js

---
+++ JavaScript_Advance/03_basics/01_functions.js
@@ -0,0 +1,14 @@
+function sayMyName(){
+    console.log("P");
+    console.log("R");
+    console.log("I");
+    console.log("N");
+    console.log("C");
+    console.log("E");
+}
+
+// sayMyName();
+
+function addTwoNumbers(number1,number2){
+    console.log(number1 + number2)
+}
