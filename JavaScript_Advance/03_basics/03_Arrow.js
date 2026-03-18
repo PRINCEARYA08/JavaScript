26242096b7745a9ad1364d5716d75923b61f4012
PRINCE ARYA <Parya3252@gmail.com>
2026-03-18 11:19:45 +0000

    03_arrowFunctions.js

---
+++ JavaScript_Advance/03_basics/03_Arrow.js
@@ -0,0 +1,43 @@
+const user ={
+    username : "prince",
+    price:999,
+
+    welcomeMessage: function(){
+       
+        console.log(`${this.username} , welcome to website`);
+        //console.log(this);
+    }
+
+}
+// user.welcomeMessage();
+// user.username = "sam"
+// user.welcomeMessage();
+
+// console.log(this);
+
+// function chai() {
+//     let username = "prince"
+//     console.log(this.username);
+// }
+// chai()
+
+const chai = () =>{
+    let username = "prince"
+    console.log(this);
+
+
+}
+// chai()
+
+// const addTwo = (num1, num2) =>{
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2)
+
+const addTwo = (num1, num2) => ({username: "prince"})
+
+console.log(addTwo(3,4))
+
