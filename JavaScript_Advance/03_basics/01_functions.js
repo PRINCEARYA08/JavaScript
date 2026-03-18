d35d7da19665410d5487ba95da29e8eda6dab2bc
PRINCE ARYA <Parya3252@gmail.com>
2026-03-18 10:15:46 +0000

    02_scope.js

--- JavaScript_Advance/03_basics/01_functions.js
+++ JavaScript_Advance/03_basics/01_functions.js
@@ -9,8 +9,59 @@ function sayMyName(){
 
 // sayMyName();
 
+// function addTwoNumbers(number1,number2){
+//     console.log(number1 + number2)
+// }
+
+// addTwoNumbers(5,10);
+
+
+
 function addTwoNumbers(number1,number2){
-    console.log(number1 + number2)
+
+    // let result = number1 + number2
+    // return result
+
+    return number1 + number2
 }
 
-addTwoNumbers();
+const result = addTwoNumbers(3,5)
+// console.log("Result: ", result); 
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage("Prince"))
+// console.log(loginUserMessage())
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,300,400,500))
+
+const user = {
+    username: "prince",
+    price:199
+}
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+// handleObject(user)
+// handleObject({
+//     username: "sam",
+//     price:399
+// })
+
+const myNewArray = [200,300,400]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,300,400]));
