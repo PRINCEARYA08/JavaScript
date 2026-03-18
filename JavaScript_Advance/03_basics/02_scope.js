51de731705bda4582b51777b05c5bfab7cae3798
PRINCE ARYA <Parya3252@gmail.com>
2026-03-18 10:43:35 +0000

    02_hoisting

--- JavaScript_Advance/03_basics/02_scope.js
+++ JavaScript_Advance/03_basics/02_scope.js
@@ -3,10 +3,52 @@ let a = 300
 if(true){
     let a = 10
     const b =20
-    console.log("INNER:",a);
+    // console.log("INNER:",a);
 }
 
 
-console.log(a);
+// console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+const username = "prince"
+
+  function two(){ 
+     const website = "youtube"
+    console.log(username);
+}
+
+// console.log(website)
+
+  two()
+}
+//one()
+
+if(true){
+    const username = "Prince"
+    if(username === "Prince"){
+        const website = "youtube"
+        // console.log(username + " " + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ==========+++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++++===============
+
+
+console.log(addone(5));
+function addone(rum) {
+    return rum + 1;
+}
+// console.log(addone(5));
+
+
+
+// console.log(addtwo(5));  // in case of function expression we cannot call the function before its declaration because of hoisting
+const addtwo = function(rum) {
+    return rum + 2;
+}
+ 
+console.log(addtwo(5));
