2e355d885d833765b60dda3cac1a96640a7be533
PRINCE ARYA <Parya3252@gmail.com>
2026-03-20 14:07:13 +0000

    truthly

---
+++ JavaScript_Advance/04_control_flow/truthy.js
@@ -0,0 +1,26 @@
+// const userEmail = "paryay@.ai"
+// const userEmail = ""
+// if(userEmail){
+//     console.log("Got email id")
+// }
+// else{
+//     console.log("Don't have user email ")
+// }
+
+// falsy values in js
+// false, 0, -0, 0n, "", null, undefined, NaN
+
+// truthy values in js
+// true, {}, [], " ", 1, -1, 3.14, "false", "0", "null", "undefined", "NaN", function(){}, Symbol(), Infinity, -Infinity
+
+
+
+// if(userEmail.length === 0){
+//     console.log("array is empty")
+// }
+    
+const emptyObj = {} 
+ 
+if (Object.keys(emptyObj).length === 0){
+    console.log("object is empty")
+}
