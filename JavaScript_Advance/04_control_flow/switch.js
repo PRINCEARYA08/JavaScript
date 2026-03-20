21e0ea0e697b530a9325aa1ebff1824b1e1bcd89
PRINCE ARYA <Parya3252@gmail.com>
2026-03-20 13:02:05 +0000

    swich

---
+++ JavaScript_Advance/04_control_flow/switch.js
@@ -0,0 +1,38 @@
+// switch (key) {
+//     case value1:
+//         // code block
+//         break;
+//     case value2:
+//         // code block
+//         break;
+//     default:
+//         // code block
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+     console.log("January")
+      break;
+    case 2:
+     console.log("February")
+      break;
+    case 3:
+     console.log("March")
+      break;
+    case 4:
+     console.log("April")
+      break;
+    case 5:
+     console.log("May")
+      break;
+    case 6:
+     console.log("Jun")
+      break;
+    
+     default:
+      console.log("default")
+        break;
+}
+
