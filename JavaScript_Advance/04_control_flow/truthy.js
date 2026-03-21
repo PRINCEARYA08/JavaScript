82cd05c41520a14f7214570476cdb04fc7a5ef27
PRINCE ARYA <Parya3252@gmail.com>
2026-03-21 09:00:30 +0000

    truthly.js

--- JavaScript_Advance/04_control_flow/truthy.js
+++ JavaScript_Advance/04_control_flow/truthy.js
@@ -19,8 +19,23 @@
 //     console.log("array is empty")
 // }
     
-const emptyObj = {} 
+// const emptyObj = {} 
  
-if (Object.keys(emptyObj).length === 0){
-    console.log("object is empty")
-}
+// if (Object.keys(emptyObj).length === 0){
+//     console.log("object is empty")
+// }
+
+// nullish coalescing Operator (??): null undifined
+
+let val1;
+val1= 5??10
+val1= null??10
+val1= undefined??10
+val1= null??20??10
+console.log(val1)
+// 10
+
+// Terniary operator
+
+const iceTeaPrice = 100
+iceTeaPrice <=80  ? console.log("less than 80"): console.log("more than 80")
