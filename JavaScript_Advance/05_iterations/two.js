d67316b2abc2526a1a40db3d0451034e7b2239cf
PRINCE ARYA <Parya3252@gmail.com>
2026-03-21 09:49:26 +0000

    while_and_dowhile

---
+++ JavaScript_Advance/05_iterations/two.js
@@ -0,0 +1,29 @@
+// while
+let index = 1;
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index= index + 2;
+}
+
+let myArray = ["badshah", "yo yo honey singh", "eminem", "drake", "kendrick lamar"]
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(`the value is ${myArray[arr]}`);
+    arr = arr + 1;
+   
+}
+
+
+// let score = 1;
+// do {
+//     console.log(`the score is ${score}`);
+//     score = score + 1;
+// } while (score <= 10);
+
+
+
+let num = 15;  // first time it will execute the block of code and then it will check the condition
+do {
+    console.log(`the num is ${num}`);
+    num = num + 1;
+} while (num <= 10);
