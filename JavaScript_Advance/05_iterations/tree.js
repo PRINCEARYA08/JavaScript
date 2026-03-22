9e330584b9b624a0fed27e60f4f52fd59002b4d4
PRINCE ARYA <Parya3252@gmail.com>
2026-03-22 09:43:11 +0000

    tree.js up

--- JavaScript_Advance/05_iterations/tree.js
+++ JavaScript_Advance/05_iterations/tree.js
@@ -6,8 +6,32 @@
 const arr = [1,2,3,4,5]
 
 for(const num of arr){
-console.log(num);
+// console.log(num);
 }
 
-// const greetings = "hello world!"
-// for
+const greetings = "hello world!"
+for(const greet of greetings){
+    // console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+console.log(map);
+
+for(const [key,value] of map){
+    console.log(key,':-',value);
+}
+
+const myObject ={
+    'game1':'nfs',
+    'game2':'spiderman'
+}
+// for(const [key,value]of myObject){
+//     console.log(key,':-',value);
+// }
