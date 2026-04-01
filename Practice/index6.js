5055d02e08b14e85a98f29309b136b7abdaedbb2
PRINCE ARYA <Parya3252@gmail.com>
2026-04-01 11:26:26 +0000

    index6.js

---
+++ Practice/index6.js
@@ -0,0 +1,24 @@
+const shoppingCart = [
+    {
+        item : "js",
+    price: 1000
+},
+{
+        item : "java",
+    price: 2000
+},
+{
+        item : "jova",
+    price: 3000
+},
+{
+        item : "joni",
+    price: 4000
+},
+{
+        item : "jani",
+    price: 5000
+},
+
+
+]
