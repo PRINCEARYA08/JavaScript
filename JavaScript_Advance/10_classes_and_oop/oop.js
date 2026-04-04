43e1d63c39749d44ff74a334bbb2c36e1f37ce59
PRINCE ARYA <Parya3252@gmail.com>
2026-04-04 09:54:53 +0000

    oop

---
+++ JavaScript_Advance/10_classes_and_oop/oop.js
@@ -0,0 +1,13 @@
+const user ={
+    username: "Prince",
+    loginCount: 8,
+    signedIn: true,
+
+
+    getUserDetails: function(){
+        console.log("Got user details from database");
+    }
+
+}
+console.log(user.username);
+console.log(user.getUserDetails);
